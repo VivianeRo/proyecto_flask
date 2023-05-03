fae3d1a27f5d3aff284c1c75acfe4344706328ca
Viviane Rodriguez <vivianerg84@gmail.com>
2023-05-03 11:24:44 -0300

    se creo un nuevo archivo

---
+++ persona.py
@@ -0,0 +1,15 @@
+class Persona:
+    def __init__(self,dni,nombre):
+        self.dni = dni
+        self.nombre = nombre
+
+
+    def __srt(self):
+        return f'Nombre :'{self.nombre}
+    def saludo (self):
+        return f'Hola'{self.nombre}
+
+
+
+    
+
--- proyecto_flask/app.py
+++ proyecto_flask/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', saludo = persona1.saludo())
 
 if __name__ == '__main__':
+
+    from persona import Persona
+    persona1 = Persona(356783,'Viviane')
+    print(persona1.saludo())
     app.run(debug=True)
